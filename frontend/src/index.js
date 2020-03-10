71e8e511db89cf87a6e8ad2c1bcbc9324bb2b016
kjdowns <kevin.downs04@gmail.com>
2020-03-10 19:09:13 -0400

    add temporary testing code for player movement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,49 @@
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d")
 let background = new Image();
-background.src = "img/dungeon_entrance.JPG"
-background.onload = function(){
-    ctx.drawImage(background,0,0);
+document.addEventListener('keydown', function(e){
+    if (e.key == "ArrowLeft") {
+        console.log("Arrow Left pressed")
+        player.x -= player.speed;
+    } else if (e.key == "ArrowRight") {
+        console.log("Arrow Right pressed")
+        player.x += player.speed
+    } else if (e.key == "ArrowDown") {
+        console.log("Arrow Down pressed")
+        player.y += player.speed
+    } else if (e.key == "ArrowUp") {
+        console.log("Arrow Up pressed")
+        player.y -= player.speed
+    }
+})
 
-    // Render test player box
-    let player = new Path2D();
-    player.rect(390, 410, 20, 35); 
-    ctx.stroke(player); 
-    ctx.fill(player);
+// Add character and movement testing
 
-    //Move test player box
-    
+let player = {
+        x: 390,
+        y: 400,
+        speed: 8,
+        width: 20,
+        height: 20,
+        color: 'green',
+};
+
+function initializeScene() {
+    background.src = "img/dungeon_entrance.JPG"
+    background.onload = function(){
+    ctx.drawImage(background, 0, 0);
+    drawEntity(player);
+    }
 }
 
+function drawEntity(entity) {
+    ctx.save();
+    ctx.fillStyle = entity.color;
+    ctx.fillRect(entity.x, entity.y, entity.width, entity.height);
+    ctx.restore();
+}
 
+function gameLoop() {
+    initializeScene();
+}
+setInterval(gameLoop,15);
