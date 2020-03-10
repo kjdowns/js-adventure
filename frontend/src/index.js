29a9f251fcfe98862072c75c62733883724dceb3
kjdowns <kevin.downs04@gmail.com>
2020-03-10 15:29:15 -0400

    add and render player text box to canvas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,20 @@
+// Display canvas backround test code
+
 const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d")
 let background = new Image();
 background.src = "img/dungeon_entrance.JPG"
 background.onload = function(){
-    ctx.drawImage(background,0,0);   
+    ctx.drawImage(background,0,0);
+
+    // Render test player box
+    let player = new Path2D();
+    player.rect(390, 410, 20, 35); 
+    ctx.stroke(player); 
+    ctx.fill(player);
+
+    //Move test player box
+    
 }
+
+
