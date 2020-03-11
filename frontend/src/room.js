219b4d8945c63e61b8b201485572a7deda7afcf0
kjdowns <kevin.downs04@gmail.com>
2020-03-11 15:42:19 -0400

    add map boundary to room

--- frontend/src/room.js
+++ frontend/src/room.js
@@ -2,6 +2,10 @@ class Room {
     constructor(src){
         this.background = new Image();
         this.background.src = src
+        this.leftBoundary = 40; //x left
+        this.rightBoundary = 760; //x right
+        this.topBoundary = 65; // y top
+        this.bottomBoundary = 440; //y bottom
     }
 
     render(){
