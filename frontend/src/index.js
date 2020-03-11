557a3f6a646dc66d5bb5fb13da9b64204dcb25fb
kjdowns <kevin.downs04@gmail.com>
2020-03-11 13:36:15 -0400

    move player logic into its own player class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,32 +4,24 @@ const gameCanvas = document.getElementById("gameCanvas");
 const ctx = gameCanvas.getContext("2d")
 let background = new Image();
 let playerSprite = new Image();
-playerSprite.src = "img/test-sprite.png"
+playerSprite.src = "img/sprite-sheet.png"
 document.addEventListener('keydown', function(e){
     if (e.key == "ArrowLeft") {
         console.log("Arrow Left pressed")
-        player.x -= player.speed;
+        player.xPosition -= player.speed;
     } else if (e.key == "ArrowRight") {
         console.log("Arrow Right pressed")
-        player.x += player.speed
+        player.xPosition += player.speed
     } else if (e.key == "ArrowDown") {
         console.log("Arrow Down pressed")
-        player.y += player.speed
+        player.yPosition += player.speed
     } else if (e.key == "ArrowUp") {
         console.log("Arrow Up pressed")
-        player.y -= player.speed
+        player.yPosition -= player.speed
     }
 })
 
-// Add character and movement testing
-
-let player = {
-        x: 390,
-        y: 400,
-        speed: 8,
-        width: 50,
-        height: 70
-};
+let player = new Player();
 
 function initializeScene() {
     background.src = "img/dungeon_entrance.JPG"
@@ -40,7 +32,7 @@ function initializeScene() {
 }
 
 function drawEntity(entity, img) {
-    ctx.drawImage(img, 0, 0, 70, 50, entity.x, entity.y, entity.height, entity.width)
+    ctx.drawImage(img, 0, 0, 35, 35, entity.xPosition, entity.yPosition, entity.width, entity.height)
 }
 
 function gameLoop() {
---
+++ frontend/src/player.js
@@ -0,0 +1,9 @@
+class Player {
+    constructor(){
+        this.xPosition = 380;
+        this.yPosition = 400;
+        this.speed = 8;
+        this.width = 50;
+        this.height = 50;
+    }
+}
