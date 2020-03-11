e4bedc306ec522b5065af2d97b8bff4e4a2fac9a
kjdowns <kevin.downs04@gmail.com>
2020-03-11 16:09:07 -0400

    modify enemy to spawn in random location within map boundary

--- frontend/src/enemy.js
+++ frontend/src/enemy.js
@@ -2,6 +2,8 @@ class Enemy extends Player {
     constructor(name, src){
         super();
         this.name = name;
+        this.xPosition = Math.floor(Math.random() * (currentRoom.rightBoundary - currentRoom.leftBoundary)) + currentRoom.leftBoundary;
+        this.yPosition = Math.floor(Math.random() * (currentRoom.bottomBoundary - (currentRoom.topBoundary + 1))) + currentRoom.topBoundary;
         this.sprite = new Image();
         this.sprite.src = src;
     }
