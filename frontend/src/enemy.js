80cb272990977011fc9735931a5cef461897b56c
kjdowns <kevin.downs04@gmail.com>
2020-03-11 13:47:22 -0400

    create enemy class

---
+++ frontend/src/enemy.js
@@ -0,0 +1,8 @@
+class Enemy extends Player {
+    constructor(name, src){
+        super();
+        this.name = name;
+        this.this.sprite = new Image();
+        this.sprite.src = src;
+    }
+}
