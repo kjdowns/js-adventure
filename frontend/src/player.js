60e15f7b1b5afc58c812e20ca256a61dcb3b8447
kjdowns <kevin.downs04@gmail.com>
2020-03-11 16:20:13 -0400

    add collision box to player and enemy

--- frontend/src/player.js
+++ frontend/src/player.js
@@ -8,4 +8,8 @@ class Player {
         this.sprite = new Image();
         this.sprite.src = "img/sprite-sheet.png";
     }
+
+    collisionBox(){
+        return [this.xPosition, this.xPosition + this.width, this.yPosition, this.yPosition + this.height]
+    }
 }
