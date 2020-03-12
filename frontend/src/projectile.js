56cd39e7b6cdd0525dcdb03e738d84f2bcfac878
kjdowns <kevin.downs04@gmail.com>
2020-03-12 16:30:51 -0400

    add constructor to projectile class

--- frontend/src/projectile.js
+++ frontend/src/projectile.js
@@ -1,7 +1,18 @@
 class Projectile {
     
-    constructor(){
-
+    constructor(caster, type){
+        this.type = type;
+        this.xPosition = caster.xPosition;
+        this.yPosition = caster.yPosition;
+        this.direction = caster.direction;
+        this.height= 40;
+        this.width = 40;
+        this.sprite = new Image();
+        if (type == "fire") {
+            this.sprite.src = "img/fireball.png"
+        } else {
+            this.sprite.src = "img/icicle.png"
+        }
     }
-    
+
 }
