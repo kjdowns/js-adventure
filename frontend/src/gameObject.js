507ba35c78426e5663a0fe4c98b92c3feb0bacfe
kjdowns <kevin.downs04@gmail.com>
2020-03-13 17:28:28 -0400

    complete gameobject constructor

--- frontend/src/gameObject.js
+++ frontend/src/gameObject.js
@@ -1,7 +1,25 @@
 class GameObject {
     
     constructor(){
+        this.xPosition;
+        this.yPosition;
+        this.height;
+        this.width;
+        this.speed;
+        this.sprite = new Image();
+        this.sprite.src;
+        this.direction;
+        this.sheetRow;
+        this.collision = false;
+    }
 
+    collisionBox(){
+        return {
+            x: this.xPosition,
+            y: this.yPosition,
+            width: this.width,
+            height: this.height
+        }
     }
 
 }
